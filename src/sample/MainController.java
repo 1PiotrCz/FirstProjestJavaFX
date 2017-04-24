cef32ad56843e0d3e6445093dea2fca10ed9e901
Piotr Czubkowski <piotrcz@o2.pl>
2017-04-24 20:19:13 +0200

    First Project.

---
+++ src/sample/MainController.java
@@ -0,0 +1,14 @@
+package sample;
+
+/**
+ * Created by Piotr on 2017-04-24.
+ */
+public class MainController {
+    public void somebodyClickAnchor(){
+
+    }
+
+    public void clickSubmit(){
+
+    }
+}
